542ac1da2fa01edae53b09c598902529a8902724
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-08-11 16:48:17 -0300

    added .h

---
+++ algo.h
@@ -0,0 +1,13 @@
+#ifndef _ALGO_H_
+#define _ALGO_H_
+
+#include "framework/CFramework.h"
+
+//! Main algorithm entry point
+/*!
+  \param argc Number of arguments passed to the program
+  \param argv Array of argc arguments passed to the program
+*/
+void algorithm(int argc, char **argv);
+
+#endif
