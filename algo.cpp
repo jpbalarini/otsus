70c4a2d18fca65b8397d8398cde62e3811834f7c
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-08-12 22:05:58 -0300

    added .cpp

---
+++ algo.cpp
@@ -0,0 +1,13 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
+
+//
+#include "algo.h"
+//
+#include "framework/CImage.h"
+#include "framework/libparser.h"
+
+
